7da30bffe93a93b1a9448112069565ba2b921238
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-26 22:15:22 +0200

    Upgrade to Kotlin 1.5.20 etc.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
     }
 }
 plugins {
-    id("com.gradle.enterprise") version "3.6.2"
-    id("de.fayard.refreshVersions") version "0.10.0"
+    id("com.gradle.enterprise") version "3.6.3"
+    id("de.fayard.refreshVersions") version "0.10.1"
     id("com.louiscad.complete-kotlin") version "1.0.0"
 }
 
